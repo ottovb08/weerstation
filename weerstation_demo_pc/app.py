b7cd432b83518effb72099e647875b11fc0d1398
Jazper17 <jasperraemaekers@gmail.com>
2025-10-24 09:21:11 +0200

    uitleg bij update

--- weerstation_demo_pc/app.py
+++ weerstation_demo_pc/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-import random
+import random, time
 
 app = Flask(__name__)
 
@@ -9,14 +9,16 @@ def home():
 
 @app.route('/data')
 def data():
-    # Simuleer live weerdata
-    return jsonify({
-        "temperature": random.uniform(15, 30),
-        "pressure": random.uniform(720, 760),
-        "wind_speed": random.uniform(0, 50),
-        "wind_direction": random.choice(["Noorden", "Oosten", "Zuiden", "Westen", "Noordoosten", "Zuidwesten"])
-    })
+    data = {
+        "temperatuur": round(random.uniform(15, 25), 2),
+        "luchtdruk": round(random.uniform(990, 1030), 2),
+        "windsnelheid": round(random.uniform(0, 25), 2),
+        "windrichting": random.choice(["Noorden", "Noordoosten", "Oosten", "Zuidoosten", "Zuiden", "Zuidwesten", "Westen", "Noordwesten"]),
+        "windhoek": random.randint(0, 359),
+        "tijd": time.strftime("%H:%M:%S")
+    }
+    return jsonify(data)
 
 if __name__ == '__main__':
-    print("Server gestart op: http://127.0.0.1:5000")
-    app.run(debug=True)
+    print("🌦  Server gestart op: http://127.0.0.1:5000")
+    app.run(host='0.0.0.0', port=5000)
